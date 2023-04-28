ad2d7d8688b68cf24e1985a9acc8b5f0512676e0
Michal <michalg2105@gmail.com>
2023-04-28 13:19:10 +0200

    das

---
+++ Snake/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        SnakeBoard game;
+        private Graphics g;
+        private const int cellSize=25;
+        bool gameOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+            setGame();
+
+        }
+        private void setGame()
+        {
+            gameOver = false;
+            game = new SnakeBoard(new Size(20, 20));
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Padding = new Padding(0);
+            pictureBox1.Width = cellSize * game.size.Width;
+            pictureBox1.Height = cellSize * game.size.Height;
+
+            pictureBox1.Image = new Bitmap(game.size.Width * cellSize, game.size.Height * cellSize);
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            g = Graphics.FromImage(pictureBox1.Image);
+            DrawBoard();
+            game.BoardChanged += DrawBoard;
+            game.GameIsOver += MakeGameReapeatable; 
+        }
+        private void MakeGameReapeatable()
+        {
+            gameOver = true;
+        }
+        private void DrawBoard()
+        {
+            g.Clear(Color.White);
+            for(int i=0;i< game.size.Width; i++)
+            {
+                for(int j=0;j< game.size.Height; j++)
+                {
+                    g.DrawRectangle(new Pen(Color.Black), i * cellSize, j * cellSize, cellSize, cellSize);
+                }
+            }
+            bool firstOne = true;
+            foreach(Point c in game.getSnake())
+            {
+                if (firstOne)
+                {
+                    firstOne = false;
+                    g.FillRectangle(Brushes.Green, c.X * cellSize, c.Y * cellSize, cellSize, cellSize);
+                    g.DrawRectangle(new Pen(Color.Black), c.X * cellSize, c.Y * cellSize, cellSize, cellSize);
+                    continue;
+                }
+                g.FillRectangle(Brushes.LightGreen, c.X * cellSize, c.Y * cellSize, cellSize, cellSize);
+                g.DrawRectangle(new Pen(Color.Black), c.X * cellSize, c.Y * cellSize, cellSize, cellSize);
+            }
+            foreach(Point c in game.drawObstacles())
+            {
+                g.FillRectangle(Brushes.SaddleBrown, c.X * cellSize, c.Y * cellSize, cellSize, cellSize);
+                g.DrawRectangle(new Pen(Color.Black), c.X * cellSize, c.Y * cellSize, cellSize, cellSize);
+            }
+            g.FillRectangle(Brushes.Red, game.getApple().X* cellSize, game.getApple().Y* cellSize, cellSize, cellSize);
+            g.DrawRectangle(new Pen(Color.Black), game.getApple().X* cellSize, game.getApple().Y* cellSize, cellSize, cellSize);
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && game.snake.blocked() != Snake.Direction.LEFT) { game.snake.dir = Snake.Direction.LEFT; }
+            if (e.KeyCode == Keys.D && game.snake.blocked() != Snake.Direction.RIGHT) { game.snake.dir = Snake.Direction.RIGHT; }
+            if (e.KeyCode == Keys.W && game.snake.blocked() != Snake.Direction.UP) { game.snake.dir = Snake.Direction.UP; }
+            if (e.KeyCode == Keys.S && game.snake.blocked() != Snake.Direction.DOWN) { game.snake.dir = Snake.Direction.DOWN; }
+            if (e.KeyCode == Keys.R && gameOver) setGame();
+        }
+    }
+}
---
+++ Snake/Plansza.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class SnakeBoard
+    {
+        int punkty;
+        Point apple;
+        public Snake snake;
+        public Size size { get; }
+        public event Action BoardChanged;
+        public event Action GameIsOver;
+        Timer timer;
+        List<Point> obstacles;
+
+        public SnakeBoard(Size sizeN)
+        {
+            obstacles = new List<Point>();
+            punkty = 0;
+            size = sizeN;
+            
+            List<Point> tmp = new List<Point>();
+            
+            tmp.Add(new Point(size.Width / 2, size.Height / 2 - 1));
+            tmp.Add(new Point(size.Width / 2, size.Height / 2));
+            tmp.Add(new Point(size.Width / 2, size.Height / 2 + 1));
+            tmp.Add(new Point(size.Width / 2, size.Height / 2 + 2));
+            tmp.Add(new Point(size.Width / 2, size.Height / 2 + 3));
+            snake = new Snake(tmp);
+            timer = new Timer();
+            generateApple();
+            timer.Interval = 220;
+            timer.Start();
+            timer.Tick += Timer_Tick; ;
+
+
+        }
+        private void generateApple()
+        {
+            Random rnd = new Random();
+            bool gotGoodOne = false;
+            while (!gotGoodOne)
+            {
+                gotGoodOne = true;
+                apple = new Point(rnd.Next(0, size.Width), rnd.Next(0, size.Height));
+                foreach (Point c in snake.segments)
+                {
+                    if (c.X == apple.X && c.Y == apple.Y)
+                    {
+                        gotGoodOne = false;
+                        break;
+                    }
+                }
+                if (obstacles.Count > 0)
+                {
+                    foreach (Point c in obstacles)
+                    {
+                        if (c.X == apple.X && c.Y == apple.Y)
+                        {
+                            gotGoodOne = false;
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+        public Point getApple()
+        {
+            return apple;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Point x=new Point(0,0);
+            Point returnMe= snake.segments[snake.segments.Count - 1];
+            snake.segments.RemoveAt(snake.segments.Count - 1);
+            if (snake.dir == Snake.Direction.LEFT)
+            {
+                x = new Point(snake.segments[0].X-1, snake.segments[0].Y);
+                foreach (Point c in snake.segments)
+                {
+                    if (c.X == x.X && c.Y == x.Y)
+                    {
+                        timer.Stop(); GameIsOver?.Invoke();
+                        return;
+                    }
+                }
+                snake.segments.Insert(0,x);
+
+            }
+            if (snake.dir == Snake.Direction.RIGHT)
+            {
+                x = new Point(snake.segments[0].X + 1, snake.segments[0].Y);
+                foreach (Point c in snake.segments)
+                {
+                    if (c.X == x.X && c.Y == x.Y)
+                    {
+                        timer.Stop(); GameIsOver?.Invoke();
+                        return;
+                    }
+                }
+                snake.segments.Insert(0, x);
+            }
+            if (snake.dir == Snake.Direction.DOWN)
+            {
+                x = new Point(snake.segments[0].X, snake.segments[0].Y + 1);
+                foreach (Point c in snake.segments)
+                {
+                    if (c.X == x.X && c.Y == x.Y)
+                    {
+                        timer.Stop(); GameIsOver?.Invoke();
+                        return;
+                    }
+                }
+                snake.segments.Insert(0, x);
+
+            }
+            if (snake.dir == Snake.Direction.UP)
+            {
+                x = new Point(snake.segments[0].X, snake.segments[0].Y - 1);
+                foreach (Point c in snake.segments)
+                {
+                    if(c.X==x.X&&c.Y==x.Y)
+                    {
+                        timer.Stop(); GameIsOver?.Invoke();
+                        return;
+                    }
+                }
+                snake.segments.Insert(0, x);
+
+            }
+            
+            if (x.X == apple.X && x.Y == apple.Y)
+            {
+                snake.segments.Add(returnMe);
+                punkty++;
+                 generateObstacle();
+                generateApple();
+                if(timer.Interval>50)timer.Interval -= 5;
+            }
+            if (snake.segments[0].Y < 0 || snake.segments[0].Y >= size.Height)
+            {
+                timer.Stop(); GameIsOver?.Invoke();
+                return;
+            }
+            if (snake.segments[0].X < 0 || snake.segments[0].X >= size.Width)
+            {
+                timer.Stop();
+                GameIsOver?.Invoke();
+                return;
+            }
+            foreach(Point obstacle in obstacles)
+            {
+                if(obstacle.X==snake.segments[0].X&& obstacle.Y == snake.segments[0].Y)
+                {
+                    timer.Stop();
+                    GameIsOver?.Invoke();
+                    return;
+                }
+            }
+           
+
+            BoardChanged?.Invoke();
+        }
+        public List<Point> drawObstacles()
+        {
+            return obstacles;
+        }
+        private void generateObstacle()
+        {
+            Random rnd = new Random();
+            bool gotGoodOne = false;
+            int howMany = rnd.Next(0, 6);
+            Point addMeToObstacles;
+            int added = 0;
+                while (added!=howMany)
+                {
+                    
+                    gotGoodOne = true;
+                    addMeToObstacles = new Point(rnd.Next(0, size.Width), rnd.Next(0, size.Height));
+                    foreach (Point c in snake.segments)
+                    {
+                        if (c.X == addMeToObstacles.X && c.Y == addMeToObstacles.Y)
+                        {
+                            gotGoodOne = false;
+                            break;
+                        }
+                    }
+                    foreach (Point c in obstacles)
+                    {
+                        if (c.X == addMeToObstacles.X && c.Y == addMeToObstacles.Y)
+                        {
+                            gotGoodOne = false;
+                            break;
+                        }
+                    }
+                if (gotGoodOne)
+                {
+                    obstacles.Add(addMeToObstacles);
+                    added++;
+                }
+               }
+            
+            
+        }
+        
+
+        public List<Point> getSnake()
+        {
+            return snake.segments;
+        }
+
+    }
+    class Snake
+    {
+        public List<Point> segments;
+        
+        public enum Direction {UP,DOWN,RIGHT,LEFT};
+        public Direction blocked()
+        {
+           
+            if (segments.Count > 2)
+            {
+                int c = segments[0].X - segments[1].X;
+                if (c != 0)
+                {
+                    if (c > 0) return Direction.LEFT;
+                    else return Direction.RIGHT;
+                }
+                c = segments[0].Y - segments[1].Y;
+                if (c != 0)
+                {
+                    if (c < 0) return Direction.DOWN;
+                    else return Direction.UP;
+                }
+            }
+
+            return Direction.DOWN;
+        }
+        public Direction dir;
+        public Snake(List<Point> startPosition)
+        {
+            dir = Direction.UP;
+            segments = startPosition;
+        }
+
+    }
+}
